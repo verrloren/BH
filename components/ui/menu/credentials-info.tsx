1a764881c18068b3ceb2d8956b55289280ea735e
verloren <danil.koval.32000@gmail.com>
2025-06-20 23:27:03 +0400

    credentials fix

--- components/ui/menu/credentials-info.tsx
+++ components/ui/menu/credentials-info.tsx
@@ -6,13 +6,13 @@ export function CredentialsInfo() {
 	return (
 		<div className="flex flex-row items-start justify-between">
 			<div className="flex items-center gap-x-2">
-				<Phone size={18} className=" text-neutral-600" />
+				<Phone size={18} className=" text-neutral-400" />
 				<p className="text-sm md:text-md xl:text-xl text-neutral-600">+995 551 721 313</p>
 			</div>
 
 			<div className="flex items-center mb-4 gap-x-2">
 				<LucideInstagram size={18} className="text-neutral-600" />
-				<a href="https://www.instagram.com/beerhousebatumi" className="text-sm cursor-pointer md:text-md xl:text-xl text-neutral-600">beerhousebatumi</a>
+				<a href="https://www.instagram.com/beerhousebatumi" className="text-sm cursor-pointer md:text-md xl:text-xl text-neutral-400">beerhousebatumi</a>
 			</div>
 		</div>
 	)
